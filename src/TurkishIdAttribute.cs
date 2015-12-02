d9ee7c0704eb684c52aa85ee4f916e917a4ae69c
Sedat Kapanoglu <Sedat Kapanoglu>
2015-12-01 17:53:53 -0800

    Add validation attribute

---
+++ src/TurkishIdAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TurkishId
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class TurkishIdAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+            return TurkishIdNumber.IsValid(value.ToString());
+        }
+    }
+}
---
+++ test/TurkishIdAttributeTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using MbUnit.Framework;
+using TurkishId;
+
+namespace TurkishIdNumberTest
+{    
+    [TestFixture]
+    class TurkishIdAttributeTest
+    {
+        private class Dummy
+        {
+            [TurkishId]
+            public string Id { get; set; }
+        }
+
+        [Test]
+        [Row("1", false)]
+        [Row("19191919190", true)]
+        [Row("19191919191", false)]
+        [Row(19191919190, true)]
+        [Row(19191919191, false)]
+        void TurkishIdAttribute_ReturnsExpectedResult(object value, bool expectedResult)
+        {
+            var context = new ValidationContext(value, null, null);
+            var results = new List<ValidationResult>();
+            bool isValid = Validator.TryValidateValue(value, context, results, new[] { new TurkishIdAttribute() });
+            Assert.AreEqual(expectedResult, isValid);
+        }
+    }
+}
