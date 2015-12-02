75223acb75ac6a80acd2f480b2406f806c9dd07b
Sedat Kapanoglu <ssg@sourtimes.org>
2015-12-02 12:38:54 -0800

    Update nuspec

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -3,32 +3,11 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: CLSCompliant(true)]
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("TurkishId")]
 [assembly: AssemblyDescription("Turkish ID Number Validation Library")]
 [assembly: AssemblyCompany("Sedat Kapanoglu")]
 [assembly: AssemblyProduct("TurkishId")]
 [assembly: AssemblyCopyright("Copyright © Sedat Kapanoglu 2014")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("fc77467b-3330-4cdd-bd11-75e9fcf3d6a6")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.*")]
