4fbf74057a8469b5b5aebb5a727e1a9d348a1de9
Sedat Kapanoglu <ssg@sourtimes.org>
2019-02-01 19:05:56 -0800

    fix FxCop & StyleCop issues

--- src/TurkishId/TurkishIdAttribute.cs
+++ src/TurkishId/TurkishIdAttribute.cs
@@ -1,11 +1,24 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿// <copyright file="TurkishIdAttribute.cs" company="Sedat Kapanoglu">
+// Copyright (c) 2014-2019 Sedat Kapanoglu
+// Licensed under Apache-2.0 License, see LICENSE.txt for details
+// </copyright>
 
 namespace TurkishId
 {
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// Validation attribute for Turkish ID numbers
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public sealed class TurkishIdAttribute : ValidationAttribute
     {
+        /// <summary>
+        /// Check if given input is a valid Turkish ID number
+        /// </summary>
+        /// <param name="value">Input value</param>
+        /// <returns>Whether the input is a valid Turkish ID number</returns>
         public override bool IsValid(object value)
         {
             return TurkishIdNumber.IsValid(value?.ToString());
--- src/TurkishId/TurkishIdNumber.cs
+++ src/TurkishId/TurkishIdNumber.cs
@@ -1,37 +1,120 @@
-﻿/*
-     Copyright 2014 Sedat Kapanoglu
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
-
-using System;
-using System.Runtime.CompilerServices;
+﻿// <copyright file="TurkishIdNumber.cs" company="Sedat Kapanoglu">
+// Copyright (c) 2014-2019 Sedat Kapanoglu
+// Licensed under Apache-2.0 License, see LICENSE.txt for details
+// </copyright>
 
 namespace TurkishId
 {
+    using System;
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// Represents a valid Turkish ID number
+    /// </summary>
     public class TurkishIdNumber
     {
+        /// <summary>
+        /// Length of a Turkish ID number
+        /// </summary>
         public const int Length = 11;
 
-        public string Value { get; }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TurkishIdNumber"/> class.
+        /// </summary>
+        /// <param name="number">Turkish ID number</param>
         public TurkishIdNumber(string number)
         {
             if (!IsValid(number))
             {
                 throw new ArgumentException("Not a valid Turkish ID number", nameof(number));
             }
+
             this.Value = number;
+        }
+
+        /// <summary>
+        /// Gets the value of Turkish ID number
+        /// </summary>
+        public string Value { get; }
+
+        /// <summary>
+        /// A Turkish ID number can always be converted to a string
+        /// </summary>
+        /// <param name="instance">A Turkish ID number instance</param>
+        public static implicit operator string(TurkishIdNumber instance) => instance.Value;
+
+        /// <summary>
+        /// Check if given Turkish ID number is valid
+        /// </summary>
+        /// <param name="number">Input number</param>
+        /// <returns>Whether the number is a valid Turkish ID number</returns>
+        public static unsafe bool IsValid(string number)
+        {
+            if (number.Length != Length)
+            {
+                return false;
+            }
+
+            fixed (char* inputPtr = number)
+            {
+                bool invalid = false;
+                char* pInput = inputPtr;
+                int oddSum = nextDigit(ref pInput, ref invalid);
+                if (oddSum == 0 || invalid)
+                {
+                    return false;
+                }
+
+                int evenSum = 0;
+                for (int i = 0; i < 4; i++)
+                {
+                    evenSum += nextDigit(ref pInput, ref invalid);
+                    oddSum += nextDigit(ref pInput, ref invalid);
+                }
+
+                int firstChecksum = nextDigit(ref pInput, ref invalid);
+                int finalChecksum = nextDigit(ref pInput, ref invalid);
+                if (invalid)
+                {
+                    return false;
+                }
+
+                int final = (oddSum + evenSum + firstChecksum) % 10;
+                if (finalChecksum != final)
+                {
+                    return false;
+                }
+
+                int first = ((oddSum * 7) - evenSum) % 10;
+                if (first < 0)
+                {
+                    first += 10;
+                }
+
+                return firstChecksum == first;
+            }
+        }
+
+        /// <summary>
+        /// Get the string representation of the Turkish ID number
+        /// </summary>
+        /// <returns>String representation of the Turkish ID number</returns>
+        public override string ToString() => Value;
+
+        /// <summary>
+        /// Get the hash code for this instance
+        /// </summary>
+        /// <returns>Hash code</returns>
+        public override int GetHashCode() => Value.GetHashCode();
+
+        /// <summary>
+        /// Check if another object equals to this instance
+        /// </summary>
+        /// <param name="obj">Other object to compare with</param>
+        /// <returns>Whether both objects represent the same Turkish ID number</returns>
+        public override bool Equals(object obj)
+        {
+            return obj is TurkishIdNumber instance && Value == instance.Value;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -42,71 +125,8 @@ namespace TurkishId
             {
                 invalid = true;
             }
+
             return result;
-        }
-
-        /// <summary>
-        /// Check if given Turkish ID number is valid
-        /// </summary>
-        public static unsafe bool IsValid(string number)
-        {
-            if (number.Length != Length)
-            {
-                return false;
-            }
-            fixed (char* inputPtr = number)
-            {
-                bool invalid = false;
-                char* pInput = inputPtr;
-                int oddSum = nextDigit(ref pInput, ref invalid);
-                if (oddSum == 0 || invalid)
-                {
-                    return false;
-                }
-                int evenSum = 0;
-                for (int i = 0; i < 4; i++)
-                {
-                    evenSum += nextDigit(ref pInput, ref invalid);
-                    oddSum += nextDigit(ref pInput, ref invalid);
-                }
-                int firstChecksum = nextDigit(ref pInput, ref invalid);
-                int finalChecksum = nextDigit(ref pInput, ref invalid);
-                if (invalid)
-                {
-                    return false;
-                }
-                int final = (oddSum + evenSum + firstChecksum) % 10;
-                if (finalChecksum != final)
-                {
-                    return false;
-                }
-                int first = ((oddSum * 7) - evenSum) % 10;
-                if (first < 0)
-                {
-                    first += 10;
-                }
-                return firstChecksum == first;
-            }
-        }
-
-        public override string ToString()
-        {
-            return Value;
-        }
-
-        public override int GetHashCode()
-        {
-            return Value.GetHashCode();
-        }
-
-        public override bool Equals(object obj)
-        {
-            return obj is TurkishIdNumber instance && this.Value == instance.Value;
-        }
-
-        public static implicit operator string(TurkishIdNumber instance)
-        {
-            return instance.Value;
         }
     }
 }
