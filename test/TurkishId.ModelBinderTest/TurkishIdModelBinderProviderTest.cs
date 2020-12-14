1f1d5c3b18e20621a2f621d6ad3bbec038277d4b
Sedat Kapanoglu <ssg@sourtimes.org>
2020-12-13 20:02:19 -0800

    add ModelBinderProvider tests

--- test/TurkishId.ModelBinderTest/TurkishIdModelBinderProviderTest.cs
+++ test/TurkishId.ModelBinderTest/TurkishIdModelBinderProviderTest.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
 using Moq;
 using NUnit.Framework;
 using TurkishId.ModelBinder;
@@ -9,11 +9,28 @@ namespace TurkishId.ModelBinderTest
     [TestFixture]
     public class TurkishIdModelBinderProviderTest
     {
+        public abstract class FakeStringModelMetadata : ModelMetadata
+        {
+            public FakeStringModelMetadata()
+                : base(ModelMetadataIdentity.ForType(typeof(string)))
+            {
+            }
+        }
+
+        public abstract class FakeTurkishIdNumberModelMetadata : ModelMetadata
+        {
+            public FakeTurkishIdNumberModelMetadata()
+                : base(ModelMetadataIdentity.ForType(typeof(TurkishIdNumber)))
+            {
+            }
+        }
+
         [Test]
         public void GetBinder_ModelTypeIsTurkishIdNumber_ReturnsTurkishIdNumberModelBinder()
         {
+            var metadataMock = new Mock<FakeTurkishIdNumberModelMetadata>();
             var mock = new Mock<ModelBinderProviderContext>();
-            _ = mock.Setup(c => c.Metadata.ModelType).Returns(typeof(TurkishIdNumber));
+            _ = mock.SetupGet(c => c.Metadata).Returns(metadataMock.Object);
 
             var provider = new TurkishIdModelBinderProvider();
             var result = provider.GetBinder(mock.Object);
@@ -23,8 +40,9 @@ namespace TurkishId.ModelBinderTest
         [Test]
         public void GetBinder_ModelTypeIsSomethingElse_ReturnsNull()
         {
+            var metadataMock = new Mock<FakeStringModelMetadata>();
             var mock = new Mock<ModelBinderProviderContext>();
-            _ = mock.Setup(c => c.Metadata.ModelType).Returns(typeof(string));
+            _ = mock.SetupGet(c => c.Metadata).Returns(metadataMock.Object);
 
             var provider = new TurkishIdModelBinderProvider();
             var result = provider.GetBinder(mock.Object);
