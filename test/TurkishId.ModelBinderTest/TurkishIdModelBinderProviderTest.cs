f9abcd4b00debdaae187315fada6b752a9c6642c
Sedat Kapanoglu <ssg@sourtimes.org>
2020-12-13 18:15:02 -0800

    add TurkishId.ModelBinderTest project

---
+++ test/TurkishId.ModelBinderTest/TurkishIdModelBinderProviderTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Moq;
+using NUnit.Framework;
+using TurkishId.ModelBinder;
+
+namespace TurkishId.ModelBinderTest
+{
+    [TestFixture]
+    public class TurkishIdModelBinderProviderTest
+    {
+        [Test]
+        public void GetBinder_TurkishIdNumber_ReturnsTurkishIdNumberModelBinder()
+        {
+            var mock = new Mock<ModelBinderProviderContext>();
+            mock.Setup(c => c.Metadata.ModelType).Returns(typeof(TurkishIdNumber));
+
+            var provider = new TurkishIdModelBinderProvider();
+            var result = provider.GetBinder(mock.Object);
+            Assert.That(result, Is.InstanceOf<TurkishIdModelBinder>());
+        }
+    }
+}
