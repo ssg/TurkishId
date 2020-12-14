05cdcff7c7c58d1c43e8ec62710ff73feb4b2925
Sedat Kapanoglu <ssg@sourtimes.org>
2020-12-13 17:38:03 -0800

    add TurkishId.ModelBinder project

---
+++ TurkishId.ModelBinder/TurkishIdModelBinder.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Threading.Tasks;
+
+namespace TurkishId.ModelBinder
+{
+    public class TurkishIdModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            string modelName = bindingContext.ModelName;
+            var result = bindingContext.ValueProvider.GetValue(modelName);
+            if (result == ValueProviderResult.None)
+            {
+                return Task.CompletedTask;
+            }
+
+            if (!TurkishIdNumber.TryParse(result.FirstValue, out var value))
+            {
+                bindingContext.ModelState.AddModelError(modelName, "Invalid value");
+                return Task.CompletedTask;
+            }
+
+            bindingContext.Result = ModelBindingResult.Success(value);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ TurkishId.ModelBinder/TurkishIdModelBinderProvider.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+
+namespace TurkishId.ModelBinder
+{
+    public class TurkishIdModelBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder? GetBinder(ModelBinderProviderContext context)
+        {
+            return context.Metadata.ModelType == typeof(TurkishIdNumber) 
+                ? new TurkishIdModelBinder() 
+                : null;
+        }
+    }
+}
